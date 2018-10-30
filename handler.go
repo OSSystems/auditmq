a05918c0e50f5d5308e75d3323229226c998dbf2
Rodrigo de Almeida Pereira <contato@rodrigo-pereira.net>
2018-10-30 17:02:39 -0300

    Handler: integrated matcher and storage

--- handler.go
+++ handler.go
@@ -5,11 +5,30 @@ import (
 	"time"
 
 	"github.com/OSSystems/pkg/log"
+	"github.com/rodrigoapereira/auditmq/matcher"
 	"github.com/rodrigoapereira/auditmq/pkg"
+	"github.com/rodrigoapereira/auditmq/storage"
 	"github.com/streadway/amqp"
 )
 
 type Handler struct{}
+
+func (h *Handler) fillAnotherServices(actualService string, field string) {
+	for srv, serviceOpt := range matcher.Get().Services {
+		if srv == actualService {
+			continue
+		}
+
+		if !serviceOpt.FieldExist(field) {
+			log.Debugf("Field %s not configured for service %s", field, srv)
+			continue
+		}
+
+		serviceStorage := storage.GetStorageData(srv)
+		buffer := serviceStorage.GetBuffer(field)
+		buffer.CopyLastValue()
+	}
+}
 
 func (h *Handler) Handle(message amqp.Delivery) {
 	servicePayload := &pkg.Payload{}
@@ -19,6 +38,26 @@ func (h *Handler) Handle(message amqp.Delivery) {
 		message.Nack(false, true)
 		time.Sleep(5 * time.Second)
 		return
+	}
+
+	if !matcher.Get().ServiceExist(servicePayload.Service) {
+		log.Error("Service does not exists")
+		message.Nack(false, true)
+		time.Sleep(5 * time.Second)
+		return
+	}
+
+	serviceStorage := storage.GetStorageData(servicePayload.Service)
+	for field, data := range servicePayload.Data {
+		if !matcher.Get().FieldExist(field) {
+			log.Debugf("Field %s not present in configuration", field)
+			continue
+		}
+
+		buffer := serviceStorage.GetBuffer(field)
+		buffer.Push(data)
+
+		h.fillAnotherServices(servicePayload.Service, field)
 	}
 
 	message.Ack(false)
