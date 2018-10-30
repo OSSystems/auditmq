c42838d45172143b06e9b647d934192e8aed8415
Rodrigo de Almeida Pereira <contato@rodrigo-pereira.net>
2018-10-30 16:36:49 -0300

    Matcher: initial matcher structure

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/OSSystems/pkg/log"
 	"github.com/rodrigoapereira/auditmq/config"
+	"github.com/rodrigoapereira/auditmq/matcher"
 	"github.com/streadway/amqp"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+	matcher.InitializeWithData(cfg.Data)
 
 	conn := cfg.GetAMQP()
 
@@ -26,10 +28,17 @@ func main() {
 	log.Info("Listering for new messages, please use CTRL+C to stop")
 	consumer.Start()
 
+	log.Info("Initializing comparator")
+	comp := matcher.NewComparator()
+	comp.Start()
+
 	signalCh := make(chan os.Signal)
 	signal.Notify(signalCh, os.Interrupt)
 
 	<-signalCh
+	log.Info("Comparator shutdown")
+	comp.Shutdown()
+
 	log.Info("Worker shutdown")
 	err = consumer.Shutdown()
 	if err != nil {
