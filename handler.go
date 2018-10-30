37ac90fc98768c0a1c0e83451b6e983b9dbea1b5
Rodrigo de Almeida Pereira <contato@rodrigo-pereira.net>
2018-10-30 16:48:55 -0300

    Handler: added sleep for nacks

--- handler.go
+++ handler.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"time"
 
+	"github.com/OSSystems/pkg/log"
 	"github.com/rodrigoapereira/auditmq/pkg"
 	"github.com/streadway/amqp"
 )
@@ -13,13 +15,9 @@ func (h *Handler) Handle(message amqp.Delivery) {
 	servicePayload := &pkg.Payload{}
 	err := json.Unmarshal(message.Body, servicePayload)
 	if err != nil {
+		log.Error("JSON format error")
 		message.Nack(false, true)
-		return
-	}
-
-	_, err = json.Marshal(servicePayload.Data)
-	if err != nil {
-		message.Nack(false, true)
+		time.Sleep(5 * time.Second)
 		return
 	}
 
